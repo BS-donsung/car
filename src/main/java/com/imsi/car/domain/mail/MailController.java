3506f09aeec0564f7058c3bec5dc695cd561f274
Kimsw-G <kimsw3445@naver.com>
2023-03-24 17:50:30 +0900

    [진행중] mail 관련 기능 추가 메일 전송, 광고 전송

---
+++ src/main/java/com/imsi/car/domain/mail/MailController.java
@@ -0,0 +1,8 @@
+package com.imsi.car.domain.mail;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController  {
+    
+}
---
+++ src/main/java/com/imsi/car/domain/mail/MailService.java
@@ -0,0 +1,16 @@
+package com.imsi.car.domain.mail;
+
+public interface MailService {
+    
+
+    // 회원가입 인증용 이메일
+    public String joinEmail(String email);
+    
+    // 비밀번호 초기화용 이메일?
+    public String passwordEmail(String email);
+
+    // 전체 광고 메일
+    public String adsEmail(String title, String msg);
+
+    
+}
