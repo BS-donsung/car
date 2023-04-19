9f1677261d8323f04ac1b783e40704413b017318
GooDongYeon <121007224+GooDongYeon@users.noreply.github.com>
2023-04-19 11:06:02 +0900

    [완료] 문의하기 페이지 구현

--- frontend/src/view/InquiryFrm.vue
+++ frontend/src/view/InquiryFrm.vue
@@ -1,64 +1,87 @@
 <template>
   <Nav />
-  <div class="container">
-    <div class="content">
-      <div class="left-side">
-        <div class="address details">
-          <font-awesome-icon :icon="['fas', 'location-dot']" />
-          <div class="topic">
-            Address
-          </div>
-          <div class="text-one">
-            Busan, Jin-gu
-          </div>
-          <div class="text-two">
-            668 Jungang-daero, A-One Plaza 6F
+
+  <body class="body">
+    <div class="container">
+      <div class="content">
+        <div class="left-side">
+          <div class="address details">
+            <font-awesome-icon
+              :icon="['fas', 'location-dot']"
+              class="icon" />
+            <div class="topic">
+              Address
+            </div>
+            <div class="text-one">
+              Busan, Jin-gu
+            </div>
+            <div class="text-two">
+              668 Jungang-daero, A-One Plaza 6F
+            </div>
+            <div class="phone details">
+              <font-awesome-icon
+                :icon="['fas', 'phone']"
+                class="icon" />
+              <div class="topic">
+                Phone
+              </div>
+              <div class="text-one">
+                +82 010-1111-2222
+              </div>
+              <div class="text-two">
+                +82 010-2222-1111
+              </div>
+            </div>
+
+            <div class="email details">
+              <font-awesome-icon
+                :icon="['fas', 'envelope']"
+                class="icon" />
+              <div class="topic">
+                Email
+              </div>
+              <div class="text-one">
+                abcdefu@gmail.com
+              </div>
+              <div class="text-two">
+                ufedcba@gmail.com
+              </div>
+            </div>
           </div>
         </div>
 
-        <div class="phone details">
-          <font-awesome-icon :icon="['fas', 'phone']" />
-          <div class="topic">
-            Phone
+        <div class="right-side">
+          <div class="topic-text">
+            문의하기
           </div>
-          <div class="text-one">
-            +82 010-1111-2222
-          </div>
-          <div class="text-two">
-            +82 010-2222-1111
-          </div>
-        </div>
+          <p>사이트에 문의사항이 있을시 양식에 맞춰서 보내주시면, 최선을 다해 도와드리겠습니다.</p>
+          <form action="#">
+            <div class="input-box">
+              <input
+                type="text"
+                placeholder="이름을 입력하시오." />
+            </div>
 
-        <div class="email details">
-          <font-awesome-icon :icon="['fas', 'envelope']" />
-          <div class="topic">
-            Email
-          </div>
-          <div class="text-one">
-            abcdefu@gmail.com
-          </div>
-          <div class="text-two">
-            ufedcba@gmail.com
-          </div>
+            <div class="input-box">
+              <input
+                type="email"
+                placeholder="이메일을 입력하시오." />
+            </div>
+
+            <div class="input-box message-box">
+              <textarea placeholder="내용을 입력하시오."></textarea>
+            </div>
+
+            <div class="button">
+              <input
+                type="button"
+                value="보내기" />
+            </div>
+          </form>
         </div>
       </div>
-
-      <div class="right-side">
-        <div class="topic-text">
-          문의하기
-        </div>
-        <p>사이트에 문의사항이 있을시 양식에 맞춰서 보내주시면, 최선을 다해 도와드리겠습니다.</p>
-      </div>
-
-      <form action="#">
-        <div class="input-box">
-          <input
-            type="text"
-            placeholder="이름을 입력하시오." />
-        </div>
-      </form>
     </div>
-  </div>
+  </body>
 </template>
 
 <script setup>
@@ -66,7 +89,5 @@ import Nav from '@/view/ToNav.vue'
 </script>
 
 <style scoped>
-.container {
-  margin-top: 150px;
-}
+@import '@/assets/inquiry.css';
 </style>
