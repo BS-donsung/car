fc0e92c4a67a849bbcc3b560a168acb39260203d
GooDongYeon <121007224+GooDongYeon@users.noreply.github.com>
2023-05-02 15:36:24 +0900

    [완료] 메인페이지 구현

--- frontend/src/view/MainPageFrm.vue
+++ frontend/src/view/MainPageFrm.vue
@@ -1,22 +1,42 @@
 <template>
-  <User />
-  <div class="logo">
-    <router-link to="/">
-      <img
-        src="@/components/img/logo2.png"
-        alt="" />
-    </router-link>
+  <div class="main-container">
+    <User />
+    <div class="logo">
+      <router-link to="/">
+        <img
+          src="@/components/img/logo2.png"
+          alt="" />
+      </router-link>
+    </div>
+    <Nav />
+    <!-- 슬라이더 -->
+    <Slider /> 
+    <!-- 차량비교 -->
+    <Seleted />
+    <!-- 신차뉴스 / 리뷰 -->
+    <News />
+    <!-- 동영상 -->
+    <Vedio />
+    <!-- <RouterView /> -->
   </div>
-  <Nav2 />
-  <RouterView />
 </template>
 
 <script setup>
 import User from '@/components/Userpage.vue'
-import Nav2 from '@/components/NewNav.vue'
+import Nav from '@/components/NewNav.vue'
+import Slider from '@/components/ToSwiperslider.vue'
+import Seleted from '@/components/ToSelect.vue'
+import News from '@/components/ToNews.vue'
+import Vedio from '@/components/ToVideo.vue'
+
+
 </script>
 
 <style scoped>
 @import '@/assets/main.css';
 
+.main-container {
+  width: 100%;
+  height: 250vh;
+}
 </style>
